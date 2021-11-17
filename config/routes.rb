38b70980e3d4b98c6603e8067174eefb1462fbd2
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-17 06:26:14 +0530

    Added User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  
   get 'profile/edit_profile'
+  
   get 'password/forget_password'
   get 'password/reset_password'
+
   get 'account/login'
   get 'account/signup'
-  get 'home/index'
+  
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_112301) do
+ActiveRecord::Schema.define(version: 2021_11_17_005521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2021_11_15_112301) do
     t.string "designation"
     t.integer "salary"
     t.integer "experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.bigint "mobile"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
